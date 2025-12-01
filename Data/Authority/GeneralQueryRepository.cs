c42ae4b1d33005a7127dba428fd1a7048e3da4c6
bhavikgoyal <bhavikgoyal@gmail.com>
2025-12-01 19:26:07 +0530

    excute query added

---
+++ Data/Authority/GeneralQueryRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Data.Authority
+{
+    public class GeneralQueryRepository
+    {
+        public DataTable ExecuteSelectQuery(string sql)
+        {
+            using (SqlConnection conn = DatabaseHelper.GetConnection())
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+
+        public int ExecuteNonQuery(string sql)
+        {
+            using (SqlConnection conn = DatabaseHelper.GetConnection())
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
